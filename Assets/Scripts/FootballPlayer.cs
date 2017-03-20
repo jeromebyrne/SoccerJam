2d1ed4b23a7cdb6f8689f69f93c3a2af8d02f744
Jerome Byrne <jeromebyrne21@hotmail.com>
2017-03-20 13:14:17 -0700

    - first commit

---
+++ Assets/Scripts/FootballPlayer.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+
+public class FootballPlayer : MonoBehaviour 
+{
+	private Vector3 m_CurrentDirection;
+	private float m_CurrentSpeed;
+	private Rigidbody m_RigidBody = null;
+	bool mCanImpulseOnDirectionChange = true;
+	float mTimeUntilCanImpulse = 0.1f;
+	bool mIsPlayerControlled = true;
+
+	void Start () 
+	{
+		m_CurrentDirection.x = 0;
+		m_CurrentDirection.y = 0;
+		m_CurrentDirection.z = 1;
+
+		m_CurrentSpeed = 800.0f;
+	}
+
+	void Update() 
+	{
+		if (m_RigidBody) 
+		{
+			m_RigidBody.AddForce (m_CurrentDirection * m_CurrentSpeed);
+
+			// if (m_RigidBody.velocity.sqrMagnitude < 200)
+			{
+			}
+		} 
+		else
+		{
+			m_RigidBody = GetComponent<Rigidbody> ();
+		}
+
+		if (mIsPlayerControlled)
+		{
+			if (!mCanImpulseOnDirectionChange)
+			{
+				mTimeUntilCanImpulse -= Time.deltaTime;
+
+				if (mTimeUntilCanImpulse <= 0.0f)
+				{
+					mCanImpulseOnDirectionChange = true;
+				}
+			}
+		}
+	}
+
+	public static implicit operator FootballPlayer(Collider v)
+    {
+        throw new NotImplementedException();
+    }
+
+	public void SetDirection (float x, float z)
+	{
+		m_CurrentDirection.x = x;
+		m_CurrentDirection.z = z;
+
+		if (mCanImpulseOnDirectionChange)
+		{
+			m_RigidBody.AddForce (m_CurrentDirection * (m_CurrentSpeed * 0.75f), ForceMode.Impulse);
+			mCanImpulseOnDirectionChange = false;
+			mTimeUntilCanImpulse = 2.0f;
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+	public Camera m_Camera = null;
+	public FootballPlayer m_TestPlayer = null;
+	private static GameManager m_Instance;						// Damien : Making GameManager a singleton
+
+	private float m_CameraZOffset = -7.0f;
+	private float m_CameraYOffset = 10.0f;
+
+	private bool m_IsSetup = false;
+
+	public static GameManager Instance
+	{
+		get 
+		{
+			if (m_Instance == null)
+			{
+				m_Instance = new GameManager();
+			}
+			return m_Instance;
+		}
+	}
+
+	void Start () 
+	{
+		m_Instance = this;	// Hacky singleton
+
+	}
+
+	void DoSetup()
+	{
+	}
+
+	void Update () 
+	{
+		// Doing setup in Update because scene not fully created in Start()
+		if (!m_IsSetup)
+		{	
+			DoSetup ();
+			m_IsSetup = true;
+			return;
+		}
+
+		TakeInput ();
+
+		UpdateCamera ();
+	}
+
+	void TakeInput()
+	{
+		if (m_TestPlayer == null) 
+		{
+			return;
+		}
+			
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch (0);
+
+			if (touch.phase == TouchPhase.Ended)
+			{
+				/*
+				RaycastHit hit;
+
+				Ray rr = m_Camera.ScreenPointToRay (touch.position);
+
+				if (Physics.Raycast(rr, out hit) )
+				{
+					NavNode nearestNode = m_NavGrid.GetNearestNode (hit.point.x, hit.point.z);
+
+					if (nearestNode != null)
+					{
+						var path = m_NavGrid.GetPath (m_LocalPlayer.m_CurrentNavGridRow, m_LocalPlayer.m_CurrentNavGridColumn, nearestNode.m_RowIndex, nearestNode.m_ColumnIndex);
+
+						m_LocalPlayer.SetPathAndMove (path);
+					}
+				}
+				*/
+			}
+		}
+			
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+
+			Ray rr = m_Camera.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast(rr, out hit) )
+			{
+				Vector3 diff =  hit.point - m_TestPlayer.transform.position;
+				diff.Normalize ();
+
+				m_TestPlayer.SetDirection (diff.x, diff.z);
+			}
+		}
+	}
+
+	void UpdateCamera ()
+	{
+		if (m_Camera == null)
+		{
+			return;
+		}
+
+		if (m_TestPlayer == null)
+		{
+			return;
+		}
+
+		Vector3 position = m_Camera.transform.position;
+		position.x = m_TestPlayer.transform.position.x;
+		position.y = m_CameraYOffset;
+		position.z = m_TestPlayer.transform.position.z + m_CameraZOffset;
+
+		m_Camera.transform.position = position;
+				
+		/*
+	private void FindAveragePosition()
+	{
+		Vector3 averagePos = new Vector3();
+		int numTargets = 0;
+
+		for (int i = 0; i < m_Targets.Length; i++)
+		{
+			if (!m_Targets[i].gameObject.activeSelf)
+				continue;
+
+			averagePos += m_Targets[i].position;
+			numTargets++;
+		}
+
+		if (numTargets > 0)
+			averagePos /= numTargets;
+
+		averagePos.y = transform.position.y;
+
+		m_DesiredPosition = averagePos;
+	}
+
+
+	private void Zoom()
+	{
+		float requiredSize = FindRequiredSize();
+		m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+	}
+
+
+	private float FindRequiredSize()
+	{
+		Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+		float size = 0f;
+
+		for (int i = 0; i < m_Targets.Length; i++)
+		{
+			if (!m_Targets[i].gameObject.activeSelf)
+				continue;
+
+			Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+			Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+
+			size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+		}
+
+		size += m_ScreenEdgeBuffer;
+
+		size = Mathf.Max(size, m_MinSize);
+
+		return size;
+	}
+	*/
+	}
+}
